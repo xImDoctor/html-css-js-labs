581017858607292c3aea3e3c6a32bd0a83863378
Andrew ImDoctor <andrew@imdoc.ru>
2024-05-20 15:52:40 +0300

    Update lab4.js
    Update class functions

--- js-labs/lab4/lab4.js
+++ js-labs/lab4/lab4.js
@@ -1,50 +1,55 @@
 class Book {
-
     constructor(title, pubYear, price) {
-        this.title = title;
-        this.pubYear = pubYear;
-        this.price = price;
+        this._title = title;
+        this._pubYear = pubYear;
+        this._price = price;
     }
 
     get title() {
-        return this.title;
+        return this._title;
     }
 
     set title(text) {
-        if (text === '')
-            return;
-
-        this.title = text;
+        if (typeof text === 'string' && text.trim() !== '')
+            this._title = text.trim();
+        else
+            console.error('Title должен быть непустой строкой.');
     }
 
     get pubYear() {
-        return this.pubYear;
+        return this._pubYear;
     }
 
-    set pubYear(year) {
-        if (year <= 0)
-            return;
-
-        this.pubYear = year;
+    set pubYear(newPubYear) {
+        if (typeof newPubYear === 'number' && newPubYear > 0)
+            this._pubYear = newPubYear;
+        else
+            console.error('pubYear должен быть положительным числом.');
     }
 
     get price() {
-        return this.price;
+        return this._price;
     }
 
-    set price(value) {
-        if (value <= 0)
-            return;
-
-        this.price = value;
+    set price(newPrice) {
+        if (typeof newPrice === 'number' && newPrice > 0)
+            this._price = newPrice;
+        else
+            console.error('Price должен быть положительным числом.');
     }
-    
+
     show() {
-        console.log(`Название: ${this.title}, Год публикации: ${this.pubYear}, Стоимость: ${this.price}`);
+        console.log(`Title: ${this._title}, Public Year: ${this._pubYear}, Price: ${this._price}`);
     }
-
 
     static compare(book1, book2) {
         return book1.pubYear - book2.pubYear;
     }
 }
+
+
+
+const book1 = new Book('The Great Gatsby', 1925, 15.99);
+book1.show();
+
+console.log(book1._price);
